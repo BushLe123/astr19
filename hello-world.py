341d14856be41ef819f757ffbeada1cbe4e4f0a7
BushLe123 <150884879+BushLe123@users.noreply.github.com>
2024-01-09 19:48:02 -0800

    Add files via upload

---
+++ hello-world.py
@@ -0,0 +1,14 @@
+# this is a comment
+# this program will write Hello World!
+
+# this function tells the computer to print Hello World
+def PrintHelloWorld():
+	print("Hello World!")
+
+# this is the program that wil be run by python
+def main():
+	PrintHelloWorld()
+
+# executes the main program when script is run
+if __name__=="__main__":
+		main()
