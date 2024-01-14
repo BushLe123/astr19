ee841143e9149b337c612bb8872750d3ffb323fb
BushLe123 <150884879+BushLe123@users.noreply.github.com>
2024-01-13 21:57:57 -0800

    Session 2 prompt for Coding Journal 1

---
+++ journal1.2.py
@@ -0,0 +1,17 @@
+x = 1.2
+print("x =", x)
+y = 4.8
+print("y =", y)
+
+z = x+y
+print("x+y=",z)
+print("type(z):",type(z))
+
+a = 12
+print("a =", a)
+b = 15
+print("b =", b)
+
+c = a*b
+print("a*b=",c)
+print("type(c):",type(c))
